b607af4fa09a0fc80df2e936cc28d59f7a453dc9
shkim <skok1025@naver.com>
2023-12-23 23:07:05 +0900

    [SHBLOG-13] 서울 문화생활 정보데이터 블로그 상에 제공 - 시스템 파일 경로로 변경

--- src/main/java/com/cafe24/shkim30/service/SeoulInfoCollectService.java
+++ src/main/java/com/cafe24/shkim30/service/SeoulInfoCollectService.java
@@ -2,24 +2,33 @@ package com.cafe24.shkim30.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.FileCopyUtils;
 
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class SeoulInfoCollectService {
+    @Value("${resource-dir}")
+    public String RESOURCE_DIR;
     public String getCultureListStr() {
         try {
-            ClassPathResource resource = new ClassPathResource("event_list.json");
-            InputStream inputStream = resource.getInputStream();
-            byte[] bdata = FileCopyUtils.copyToByteArray(inputStream);
-            return new String(bdata, StandardCharsets.UTF_8);
+            String filePath = RESOURCE_DIR + "/event_list.json";
+
+            File file = new File(filePath);
+            FileSystemResource resource = new FileSystemResource(file);
+
+            byte[] bdata = FileCopyUtils.copyToByteArray(resource.getInputStream());
+            String jsonData = new String(bdata, StandardCharsets.UTF_8);
+
+            // jsonData에 파일 내용이 담겨있습니다.
+            return jsonData;
         } catch (IOException e) {
             // 예외 처리
             e.printStackTrace();
